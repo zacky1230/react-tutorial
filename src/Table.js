ce1da225f780aa40bd914e2c795b9b265934efcd
zacky31 <zachary31@outlook.com>
2019-12-14 18:40:08 +0800

    :sparkles: add react-tutorial

---
+++ src/Table.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react'
+
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    )
+}
+const TableBody = props => {
+    // return (
+    //     <tbody>
+    //         <tr>
+    //             <td>Charlie</td>
+    //             <td>Janitor</td>
+    //         </tr>
+    //         <tr>
+    //             <td>Mac</td>
+    //             <td>Bouncer</td>
+    //         </tr>
+    //         <tr>
+    //             <td>Dee</td>
+    //             <td>Aspiring actress</td>
+    //         </tr>
+    //         <tr>
+    //             <td>Dennis</td>
+    //             <td>Bartender</td>
+    //         </tr>
+    //     </tbody>
+    // )
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+                <td>
+                    <button onClick={() =>
+                        props.removeCharacter(index)}>
+                        Delete
+                        </button>
+                </td>
+            </tr>
+        )
+    })
+
+    return <tbody>{rows}</tbody>
+}
+
+class Table extends Component {
+    render() {
+        const { characterData, removeCharacter } = this.props
+
+        return (
+            <table>
+                <TableHeader />
+                <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+            </table>
+        )
+    }
+}
+
+export default Table
+
+/**
+ * when <code>return</code> is contained to one line,
+ * it does not need parenthneses().
+ */
+// const Table = () => {
+//     return 
+//         <table>
+//             <TableHeader />
+//             <TableBody />
+//         </table>
+
+// }
+
+// class Table extends Component {
+//     render() {
+//         return (
+//             <table>
+//                 <TableHeader />
+//                 <TableBody />
+//             </table>
+//         )
+//     }
+// }
+
+// class Table extends Component {
+//     render() {
+//         return (
+//             <table>
+//                 <thead>
+//                     <tr>
+//                         <th>Name</th>
+//                         <th>Job</th>
+//                     </tr>
+//                 </thead>
+//                 <tbody>
+//                     <tr>
+//                         <td>Charlie</td>
+//                         <td>Janitor</td>
+//                     </tr>
+//                     <tr>
+//                         <td>Mac</td>
+//                         <td>Bouncer</td>
+//                     </tr>
+//                     <tr>
+//                         <td>Dee</td>
+//                         <td>Aspiring actress</td>
+//                     </tr>
+//                     <tr>
+//                         <td>Dennis</td>
+//                         <td>Bartender</td>
+//                     </tr>
+//                 </tbody>
+//             </table>
+//         )
+//     }
+// }
+
+
