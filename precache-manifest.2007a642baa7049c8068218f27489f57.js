34f35c4bd5de29ebce4b20c9c188a98f19bdb1d7
zacky31 <zachary31@outlook.com>
2019-12-14 18:47:07 +0800

    Updates

---
+++ precache-manifest.2007a642baa7049c8068218f27489f57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d41919d90ca582b3f342f85f82e2107",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "8ed9dd6b21c76231088e",
+    "url": "/react-tutorial/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "1ab8a11d26d36a982dbe",
+    "url": "/react-tutorial/static/js/2.ea385857.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tutorial/static/js/2.ea385857.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ed9dd6b21c76231088e",
+    "url": "/react-tutorial/static/js/main.a8c3a6c5.chunk.js"
+  },
+  {
+    "revision": "b9cf8d7406673e674e0b",
+    "url": "/react-tutorial/static/js/runtime-main.af30ece7.js"
+  }
+]);
